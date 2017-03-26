e5d8d2a1b3148538c34a52ead3735ad3f2e2f723
Pradeep K P <arayofblessing@gmail.com>
2017-03-26 04:39:54 -0700

    Fixing issues with Solutions

--- test/scoping.js
+++ test/scoping.js
@@ -1,23 +1,19 @@
 describe('scoping', function () {
   it('should correctly deal with scoping `this` back to the callee', function () {
-    var mod = new Module(),
-        request;
-
-    request = function (callback) {
+    var mod = new Module(), request;
+    request = function (callback) 
+	{
       return callback();
     };
 
-    function Module () {
+    function Module () 
+	{
       this.foo = 'bar';
     }
-
-    Module.prototype.method = function() {
-      return this.foo;
-    };
-
-    Module.prototype.req = function() {
-      return request(this.method);
-    };
+	
+	Module.prototype.method = function() {return this.foo;};
+	
+    Module.prototype.req = function() {return request(this.method.bind(mod));};
 
     expect(mod.req()).toBe('bar');
   });
